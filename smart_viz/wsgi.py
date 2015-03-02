5a27bde732609582d9fdac3066621999eaca012b
Tofi Buzali <tofi.buzali@gmail.com>
2015-03-02 18:14:12 -0500

    first django commit

---
+++ smart_viz/wsgi.py
@@ -0,0 +1,13 @@
+"""
+WSGI config for smart_viz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
