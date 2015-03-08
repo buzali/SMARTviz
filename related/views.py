6c099e3d996554ffb221564c573f1387496d6443
sabakazi <sabakazi9@gmail.com>
2015-03-08 16:42:51 -0400

    faceebok + instag
    face

--- related/views.py
+++ related/views.py
@@ -2,11 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from facebookTwitterInsta import *
 
-def index(request):
+def index(request,ll=None):
     if request.method == 'GET':
-        twitter = setupTwitter()
-        q = request.GET.get('q')
-        if not q:
-            q = "The Porch Pittsburgh"
-        resp = searchOnTwitter(twitter,q)
+    	namepath = request.path
+    	print "aspp"
+    	print request.GET.get('q')
+        pathy = namepath.split("/")
+        print "mouse"
+        print pathy[2] 
+        relatedName = pathy[2]
+        if relatedName == "facebook":
+             searchQuery = pathy[3]
+             resp = searchOnFacebook(searchQuery)
+        elif relatedName == "twitter":
+             twitter = setupTwitter()
+             searchQuery = pathy[3]
+             #q = request.GET.get('q')
+             if not searchQuery:
+                 searchQuery = "The Porch Pittsburgh"
+             resp = searchOnTwitter(twitter,q)
+        elif relatedName == "instagram":
+             a= "asd"
+        elif relatedName == "places":
+             a= "asd" 
+
+
+        
         return HttpResponse(resp)
