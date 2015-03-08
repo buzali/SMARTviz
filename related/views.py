e2f58c29611635143839b04e89424770b4aad64b
sabakazi <sabakazi9@gmail.com>
2015-03-08 17:24:39 -0400

    Updated Links for related

--- related/views.py
+++ related/views.py
@@ -20,11 +20,25 @@ def index(request,ll=None):
              #q = request.GET.get('q')
              if not searchQuery:
                  searchQuery = "The Porch Pittsburgh"
-             resp = searchOnTwitter(twitter,q)
+             resp = searchOnTwitter(twitter,searchQuery)
         elif relatedName == "instagram":
-             a= "asd"
+             searchQuery = pathy[3]
+             resp = searchInsta2(searchQuery)
+        elif relatedName == "instaloco":
+             lat =  pathy[3]
+             lng =  pathy[4]
+             print lat
+             print lng
+             resp = searchInstagram(lat,lng)    
         elif relatedName == "places":
              a= "asd" 
+             lat =  pathy[3]
+             lng =  pathy[4]
+             typey = pathy[5]
+             searchTerm = pathy[6]
+             if not searchTerm:
+                 searchTerm = ""
+             resp = findPlaces(lat,lng,typey,searchTerm)
 
 
         
