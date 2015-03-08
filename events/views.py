a006d13b5144ab7bf31bdb6b9767768eaa319b63
Tofi Buzali <tofi.buzali@gmail.com>
2015-03-08 17:38:01 -0500

    Add events from meetups

--- events/views.py
+++ events/views.py
@@ -8,4 +8,8 @@ def index(request, ll=None):
     if not ll:
     	ll = '40.761662,-73.96805'
     foursquare = FoursquareEventFetcher.fetch(ll)
-    return HttpResponse(foursquare)
+    meetups = MeetupsEventFetcher.fetch(ll)
+    all_events = []
+    all_events = foursquare + meetups
+    json_all = json.dumps(all_events,default=lambda o: o.__dict__)
+    return HttpResponse(json_all)
